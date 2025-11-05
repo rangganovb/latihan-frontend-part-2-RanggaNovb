15be64e2342fe1cdd8188f4a717f0b7c58186549
rangganovb <ranggamum@gmail.com>
2025-11-05 12:02:24 +0700

    Menambahkan Konfigurasi BrowserRouter, Routes, dan import Bootstrap CSS

--- latihan-frontend/src/App.js
+++ latihan-frontend/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Menetapkan halaman Login sebagai rute utama aplikasi */}
+        <Route path="/" element={<Login />} />
+
+        {/* Rute publik untuk autentikasi pengguna (Login dan Register) */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Rute privat yang hanya dapat diakses jika pengguna memiliki token valid */}
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+
+        {/* Menampilkan halaman 404 jika rute tidak ditemukan */}
+        <Route
+          path="*"
+          element={<h3 className="text-center mt-5">404 Not Found</h3>}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
